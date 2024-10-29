40490830492f2592d3beea7d540072c2b74fbb17
gonzalo flores <gonzalofranciscoflores@gmail.com>
2024-10-28 21:07:37 -0300

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <ItemListContainer greeting="Este es mi E-Commerce de perifericos!" />
+      <ItemListContainer greeting="Este es mi E-Commerce de perifericos!!" />
     </>
   );
 }
